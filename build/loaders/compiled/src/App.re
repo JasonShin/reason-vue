f448432826eeef124f1702754ac5961d29472827
Jason Shin <visualbbasic@gmail.com>
2017-12-26 01:59:06 +1100

    Vue loader redefined by jomusa

---
+++ build/loaders/compiled/src/App.re
@@ -0,0 +1,12 @@
+
+
+
+
+
+
+
+
+let score: int = 10;
+let ageAndName = (24, "Lil' Reason");
+let my3dCoordinates = (20.0, 30.5, 100.0);
+Js.log(my3dCoordinates);
--- build/loaders/re-loader.js
+++ build/loaders/re-loader.js
@@ -1,10 +1,23 @@
-// Identity loader
-const fs = require('fs')
-const path = require('path')
-module.exports = function(source, sourcemap) {
-  // Write bundle.re into a source
-  console.log(source)
-  console.log(sourcemap)
-  fs.writeFileSync(path.join(__dirname, 'bundle.re'), source)
-  return source
+const fs = require('fs-extra');
+const path = require('path');
+const execSync = require('child_process').execSync;
+
+module.exports = function(source, map) {
+  const name = map.sources[0]
+  const sourcePath = `${map.sourceRoot}/${name}`
+
+
+  const conf = {
+    name : "reason-vue",
+    sources : [
+      { dir: `compiled/${map.sourceRoot}` }
+    ],
+    refmt: 3,
+    "bsc-flags": ["-bs-no-version-header"]
+  }
+
+  fs.outputFileSync(path.join(__dirname, `compiled/${sourcePath.replace('vue', 're')}`), source)
+  fs.outputJsonSync(path.join(__dirname, 'bsconfig.json'), conf)
+  execSync('bsb', { cwd: __dirname })
+  return fs.readFileSync(path.join(__dirname, `lib/js/compiled/${sourcePath.replace('vue', 'js')}`), 'utf8')
 };
