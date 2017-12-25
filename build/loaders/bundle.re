a451b0d0fa24e798e94edd5c18e6e2c546288d07
Jason Shin <visualbbasic@gmail.com>
2017-12-25 15:51:04 +1100

    adding loader stuff

---
+++ build/loaders/bundle.re
@@ -0,0 +1,11 @@
+
+
+
+
+
+
+
+
+let ageAndName = (24, "Lil' Reason");
+let my3dCoordinates = (20.0, 30.5, 100.0);
+Js.log(my3dCoordinates);
--- build/loaders/re-loader.js
+++ build/loaders/re-loader.js
@@ -1,5 +1,8 @@
 // Identity loader
+const fs = require('fs')
+const path = require('path')
 module.exports = function(source) {
-  console.log('checking source!!', source)
+  // Write bundle.re into a source
+  fs.writeFileSync(path.join(__dirname, 'bundle.re'), source)
   return source
 };
