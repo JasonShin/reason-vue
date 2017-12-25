e4e087409f92d500d00f346f5e389883b08266b5
Jason Shin <visualbbasic@gmail.com>
2017-12-26 02:08:11 +1100

    adding some comments

--- build/loaders/re-loader.js
+++ build/loaders/re-loader.js
@@ -2,11 +2,14 @@ const fs = require('fs-extra');
 const path = require('path');
 const execSync = require('child_process').execSync;
 
+
+
 module.exports = function(source, map) {
+  // File name
   const name = map.sources[0]
+  // source path + filename <i.e. src/App.vue>
   const sourcePath = `${map.sourceRoot}/${name}`
-
-
+  // Arbitary bsconfig
   const conf = {
     name : "reason-vue",
     sources : [
@@ -15,9 +18,12 @@ module.exports = function(source, map) {
     refmt: 3,
     "bsc-flags": ["-bs-no-version-header"]
   }
-
+  // Output .re into compiled/sourcePath<i.e. src/App.re>
   fs.outputFileSync(path.join(__dirname, `compiled/${sourcePath.replace('vue', 're')}`), source)
+  // Output arbitary bsconfig and override existing one
   fs.outputJsonSync(path.join(__dirname, 'bsconfig.json'), conf)
+  // Execute bsb
   execSync('bsb', { cwd: __dirname })
+  // Return compiled js under lib/js/compiled
   return fs.readFileSync(path.join(__dirname, `lib/js/compiled/${sourcePath.replace('vue', 'js')}`), 'utf8')
 };
